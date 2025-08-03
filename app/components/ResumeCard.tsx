368290759f3d2d948d50b1c603b2859d0ad4bdb4
Sean Currlin <sean.currlin@gmail.com>
2025-08-03 18:11:40 -0400

    update resume card and score badge

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router";
+import {Link} from "react-router-dom";
 import ScoreCircle from "~/components/ScoreCircle";
 import {useEffect, useState} from "react";
 import {usePuterStore} from "~/lib/puter";

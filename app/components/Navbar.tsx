49e5f9b34ed697bd11b7128747b2d27b28413b59
Sean Currlin <sean.currlin@gmail.com>
2025-08-04 10:17:16 -0400

    update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,23 +4,23 @@ import { TrashIcon, UserIcon } from '@heroicons/react/24/solid'
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/">
-        <p className="text-xl font-bold text-black">
-          <span className="sm:hidden">RM</span>
-          <span className="hidden sm:inline">RESUMIND</span>
-        </p>
-      </Link>
       <div className="flex items-center gap-4">
+        <Link to="/">
+          <p className="text-xl font-bold text-gradient">
+            <span className="sm:hidden">RM</span>
+            <span className="hidden sm:inline">RESUMIND</span>
+          </p>
+        </Link>
         <Link to="/wipe" className="text-xl cursor-pointer">
           <TrashIcon className="h-6 w-6" />
         </Link>
         <Link to="/auth" className="text-xl cursor-pointer">
           <UserIcon className="h-6 w-6" />
         </Link>
-        <Link to="/upload" className="primary-button w-fit">
-          Upload Resume
-        </Link>
       </div>
+      <Link to="/upload" className="primary-button w-fit">
+        Upload Resume
+      </Link>
     </nav>
   )
 }

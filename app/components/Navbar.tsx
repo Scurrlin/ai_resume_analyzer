35d4e28d87013e9950f858972af1acdef1044347
Sean Currlin <sean.currlin@gmail.com>
2025-08-04 09:33:14 -0400

    add links to wipe and auth pages

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,11 +4,19 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <Link to="/">
-        <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+        <p className="text-xl font-bold text-gradient">RESUMIND</p>
       </Link>
-      <Link to="/upload" className="primary-button w-fit">
-        Upload Resume
-      </Link>
+      <div className="flex items-center gap-4">
+        <Link to="/wipe" className="text-xl cursor-pointer">
+          🚮
+        </Link>
+        <Link to="/auth" className="text-xl cursor-pointer">
+          🔐
+        </Link>
+        <Link to="/upload" className="primary-button w-fit">
+          Upload Resume
+        </Link>
+      </div>
     </nav>
   )
 }

db5af6ca989c71756a5c63434ef3b26f6c6a2c96
Sean Currlin <sean.currlin@gmail.com>
2025-08-04 10:10:57 -0400

    update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,18 +1,21 @@
 import { Link } from "react-router";
-import { TrashIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import { TrashIcon, UserIcon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <Link to="/">
-        <p className="text-xl font-bold text-black">RESUMIND</p>
+        <p className="text-xl font-bold text-black">
+          <span className="sm:hidden">RM</span>
+          <span className="hidden sm:inline">RESUMIND</span>
+        </p>
       </Link>
       <div className="flex items-center gap-4">
         <Link to="/wipe" className="text-xl cursor-pointer">
-          <TrashIcon className="h-5 w-5" />
+          <TrashIcon className="h-6 w-6" />
         </Link>
         <Link to="/auth" className="text-xl cursor-pointer">
-          <UserCircleIcon className="h-5 w-5" />
+          <UserIcon className="h-6 w-6" />
         </Link>
         <Link to="/upload" className="primary-button w-fit">
           Upload Resume
